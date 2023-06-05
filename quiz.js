bf345d3e6af1b90d3009234877409fa14d44649b
Ana Gabriela Medina <130601008+Anagamedina@users.noreply.github.com>
2023-06-05 23:35:00 +0200

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,12 @@
+// Objeto para almacenar la información de las banderas
+const banderas = {
+  mexico: {
+    nombre: "México",
+    urlImagen: "banderas/mexico.png"
+  },
+  estadosUnidos: {
+    nombre: "Estados Unidos",
+    urlImagen: "banderas/estados-unidos.png"
+  },
+  // Agrega más banderas aquí...
+};
