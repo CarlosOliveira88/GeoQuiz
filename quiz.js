403f1f1d19f87559932d9e5387e230eb7568a371
joao carlos <joaocarlosnnx@gmail.com>
2023-06-08 20:18:12 +0200

    009

--- quiz.js
+++ quiz.js
@@ -16,8 +16,8 @@ class Jugador {
 // Clase Juego
 class Juego {
   constructor() {
-    this.banderas = []; // Arreglo para almacenar las preguntas
-    this.preguntaActual = 0; // Índice de la pregunta actual
+    this.banderas = [];
+    this.preguntaActual = 0;
     this.tiempoPregunta = 8000; // Tiempo para responder cada pregunta
     this.intervalId = null; // ID del intervalo de tiempo para la pregunta actual
     this.jugador = new Jugador(); // Instancia de la clase Jugador
@@ -48,51 +48,12 @@ class Juego {
     }, 1000);
   }
 
-  // Método para finalizar el juego
+
   finalizarJuego() {
   }
 }
 
-// Crear una instancia del juego
-const juego = new Juego();
 
-
-// Agregar preguntas al juego
-// juego.preguntas = [
-//   {
-//     tipo: 'bandera',
-//     imagen: 'ruta-a-la-imagen-1',
-//     opciones: ['Opción 1', 'Opción 2', 'Opción 3'],
-//     respuesta: 'Opción 1'
-//   },
-//   {
-//     tipo: 'pregunta',
-//     pregunta: '¿Cuál es la capital de Francia?',
-//     opciones: ['París', 'Londres', 'Roma'],
-//     respuesta: 'París'
-//   },
-//   // Agregar más preguntas...
-// ];
-function banderas() {
-  alert("has seleccionado banderas")
-}
-
-// // Iniciar el juego cuando se carga la página
-// document.addEventListener('DOMContentLoaded', () => {
-//   // Lógica para permitir al jugador seleccionar el tipo de juego
-//   const banderasButton = document.getElementById('banderas-button');
-//   const preguntasButton = document.getElementById('preguntas-button');
-
-
-//   banderasButton.addEventListener('click', () => {
-//     juego.iniciarJuego('banderas');
-//   });
-
-
-//   preguntasButton.addEventListener('click', () => {
-//     juego.iniciarJuego('preguntas');
-//   });
-// });
 
 
 
