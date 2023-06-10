e11fbaf4383e0ea359d0500ad85064eb966ef804
Ana Gabriela Medina <anaga.medina@gmail.com>
2023-06-10 10:14:11 +0200

    verificar respuesta

--- quiz.js
+++ quiz.js
@@ -116,10 +116,24 @@ class Juego {
     let botonD = document.getElementById('span-d');
     botonD.innerHTML = this.banderas[3];
   }
-  verificarRespuesta(respuesta) { //ana
-
-
+  verificarRespuesta(respuesta) {
+    clearInterval(this.intervalId); //detener el temporizador de la pregunta actual
+  
+    if (respuesta === this.banderas[0]) { //Se verifica si la respuesta x es igual a la primera opción de respuesta
+      this.jugador.sumarAcierto(); //si todo es ok, se llama al método sumarAcierto
+      console.log('Respuesta correcta!');
+    } else {
+      this.jugador.restarVida(); //si no , se llama al método restarvida
+      console.log('Respuesta incorrecta!');
+    }
+  
+    if (this.jugador.vidas <= 0) { //verificamos si la cantidad de vidas del jugador es menor o igual a cero
+      this.finalizarJuego(); //Si el jugador se queda sin vidas, llamomos al método finalizarJuego
+    } else {
+      this.iniciarJuego();
+    }
   }
+  
   // Método para iniciar el temporizador de cada pregunta
   startTimer() { // ana 
     const progressBar = document.getElementById('progress-bar');
@@ -147,81 +161,6 @@ let a = new Juego
 a.iniciarJuego()
 
 
-
-// Clase Jugador
-class Jugador {
-  constructor() {
-    this.vidas = 2;
-    this.aciertos = 0;
-  }
-  restarVida() {
-    this.vidas--;
-  }
-  sumarAcierto() {
-    this.aciertos++;
-  }
-}
-
-
-// Clase Juego
-class Juego {
-  constructor() {
-    this.banderas = []; // Arreglo para almacenar las preguntas
-    this.preguntaActual = 0; // Índice de la pregunta actual
-    this.tiempoPregunta = 8000; // Tiempo para responder cada pregunta
-    this.intervalId = null; // ID del intervalo de tiempo para la pregunta actual
-    this.jugador = new Jugador(); // Instancia de la clase Jugador
-  }
-  // Método para iniciar el juego
-  iniciarJuego() {
-
-  }
-  verificarRespuesta(respuesta) {
-    const bandera = this.banderass[this.preguntaActual];
-    const result = document.getElementById('result');
-    const nextButton = document.getElementById('next-button');
-
-    // Comprobar si la respuesta es correcta
-    if (respuesta === pregunta.respuesta) {
-      result.textContent = '¡Respuesta correcta!';
-    } else {
-      result.textContent = 'Respuesta incorrecta';
-      this.jugador.restarVida(); // Restar una vida al jugador
-    }
-
-
-
-  }
-  // Método para iniciar el temporizador de cada pregunta
-  startTimer() {
-    const progressBar = document.getElementById('progress-bar');
-    let timeLeft = this.tiempoPregunta / 1000;
-    progressBar.style.width = '100%';
-    progressBar.style.transition = `width ${timeLeft}s linear`;
-
-    this.intervalId = setInterval(() => {
-      timeLeft--;
-      progressBar.style.width = `${(timeLeft / (this.tiempoPregunta / 1000)) * 100}%`;
-
-      if (timeLeft === 0) {
-        clearInterval(this.intervalId);
-        this.verificarRespuesta(null);
-      }
-    }, 1000)
-  }
-  // Método para finalizar el juego
-  finalizarJuego() {
-  }
-}
-
-// Crear una instancia del juego
-const juego = new Juego();
-
-
-
-function banderas() {
-  alert("has seleccionado banderas")
-}
 
 
 
